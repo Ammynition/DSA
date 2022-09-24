1c5cc0245cc729952e022d841e61d963b76c5164
Ammy M <singh_ammy@outlook.com>
2022-09-23 22:08:00 -0400

    merged bst into just a header file

--- bst.h
+++ bst.h
@@ -1,3 +1,6 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
 
 template<typename T> struct treeNode{
     T data;
@@ -16,3 +19,87 @@ template <typename T> class BST {
         treeNode<T> * find(T data);
 
 };
+
+template <typename T>
+BST<T>::BST(){
+    head = nullptr;
+    numNodes = 0;
+}
+
+template <typename T>
+void BST<T>::add(T data){
+
+   treeNode<T> * newNode = new treeNode<T>;
+   newNode->data = data;
+   numNodes++;
+   if(head == nullptr){
+       head = newNode;
+   }
+   else{
+       treeNode<T> * current = head;
+       while(current!=nullptr){
+            if(data <= current->data){
+                if(current->left == nullptr){
+                    current->left = newNode;
+                    break;
+                }
+                else{
+                    current = current->left;
+                }
+            }
+            else{
+                if(current->right == nullptr){
+                    current->right = newNode;
+                    break;
+                }
+                else{
+                    current = current->right;
+                }
+            }
+       }
+   }
+}
+
+
+template <typename T>
+void BST<T>::print(){
+    treeNode<T> * current = head;
+    //keep track of tabs with size of parents
+    std::vector<treeNode<T> *> parents;
+    std::unordered_set<treeNode<T> *> printed;
+    while(current!=nullptr){
+        //check right
+        //if right, push to parents
+        //if right is null, check if we've printed ourselves
+        //if we haven't, then print, add to printed set,
+        //then check left, if it's there add our selves to parent set 
+        //set ourselves to left
+        //then and also if there's no left
+        //pop back the parent
+        if(current->right!=nullptr && printed.find(current->right) == printed.end()){
+            parents.push_back(current);
+            current = current->right;
+        }
+        else{
+            if(printed.find(current) == printed.end()){
+                for(int i =0; i<parents.size(); i++){
+                    std::cout << "\t";
+                }
+                std::cout << current->data << std::endl;
+                printed.insert(current);
+            }
+            if(current->left!=nullptr && printed.find(current->left) == printed.end()){
+                parents.push_back(current);
+                current = current->left;
+            }
+            else{
+                if(parents.empty()){
+                    break;
+                }
+                current = parents.back();
+                parents.pop_back();
+            }
+
+        }
+    }
+}
